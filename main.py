63376c4f74645bae4fc7a0df6dbb70e2408264cb
Kassir322 <Kassir3222@gmail.com>
2021-02-17 18:01:07 +0300

    Добавил работающее добавление файлов

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys  # sys нужен для передачи argv в QApplication
 import os  # Отсюда нам понадобятся методы для отображения содержимого директорий
 
+
 from PyQt5 import QtWidgets
 
 import main_window_design as design # Это наш конвертированный файл дизайна
@@ -13,36 +14,47 @@ class AudioRecordApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.UpdateButton.clicked.connect(self.browse_folder)
+        self.UpdateButton.clicked.connect(self.update_frame)
         
 
-    def browse_folder(self):
+    def update_frame(self):
         global frame_count
-        self.frame = QtWidgets.QFrame(self.scrollAreaWidgetContents_2)
-        frame_count += 1
-        self.frame.setObjectName('frame%d' % frame_count)
-        self.verticalLayout_5.addWidget(self.frame)
-        
-        # directory = QtWidgets.QFileDialog.getExistingDirectory(self, 'choose folder')
+        frame_count = 0
 
-        # if directory:
-        #     for file_name in os.listdir(directory):
-        #         if file_name[-3:] == 'mkv':
-        #             print(file_name)
+        while self.verticalLayout_5.count() > 0:
+            item = self.verticalLayout_5.takeAt(0)
+            check = item.widget()
+            if check:
+                check.deleteLater()
+
+        directory = QtWidgets.QFileDialog.getExistingDirectory(self, 'choose folder')
+
+        if directory:
+            for file_name in os.listdir(directory):
+                if file_name[-3:] == 'm4a':
+                    # print(file_name)
+                    self.frame = QtWidgets.QFrame(self.scrollAreaWidgetContents_2)
+                    self.frame = FileFrame()
+                    # frame_count += 1
+                    # self.frame.setObjectName('frame%d' % frame_count)
+                    self.verticalLayout_5.addWidget(self.frame)
         pass
 
 class FileFrame(QtWidgets.QFrame, frame_design.Ui_Frame):
     def __init__(self):
         super().__init__()
-        self.setupUi(self)  
+        self.setupUi(self)
 
-        print('added')
+        global frame_count
+        frame_count += 1
+        self.setObjectName('frame%d' % frame_count)
+        print(self.objectName()) 
 
 def main():
-    app = QtWidgets.QApplication(sys.argv)  # Новый экземпляр QApplication
-    window = AudioRecordApp()  # Создаём объект класса ExampleApp
-    window.show()  # Показываем окно
-    app.exec_()  # и запускаем приложение
+    app = QtWidgets.QApplication(sys.argv)
+    window = AudioRecordApp()
+    window.show()
+    app.exec_()
 
-if __name__ == '__main__':  # Если мы запускаем файл напрямую, а не импортируем
-    main()  # то запускаем функцию main()
+if __name__ == '__main__':
+    main()
