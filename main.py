687075dfa8bd184a49ab6eb55e4ee08f6cd6e7f4
Kassir322 <Kassir3222@gmail.com>
2021-02-19 10:59:13 +0300

    Добавил возможность просмотра файлов в директории

--- main.py
+++ main.py
@@ -12,21 +12,45 @@ frame_count = 0 # счетчик добавленных файлов
 frame_array = [] # массив, содержащий в себе экземпляры класса FileFrame
 directory = False
 
-def print_frame_array():
+def delete_checked():
     global frame_array
     global directory
 
     for j in range(2): # без двойного прохода почему-то не удаляются сразу 
-        for i in frame_array: # все элементы
-            i.try_to_delete()
+        for elem in frame_array: # все элементы
+            elem.try_to_delete_frames()
 
+class Dialog(QtWidgets.QFileDialog):
+    global window
+    
+    def __init__(self):
+        super().__init__()
+        self.__filters = ["All fileeees (*.*)"]
+        self.__default_filter_index = 0
+        self.__directory = ''
+
+    
+    def exec(self):
+        _filter = self.__filters[self.__default_filter_index]
+        self.setNameFilters(self.__filters)
+        self.selectNameFilter(_filter)
+        self.__directory = self.getExistingDirectory(self, 'Выберите папку', options=self.DontUseNativeDialog)
+
+
+    def get_directory(self):
+        return self.__directory
 
 class AudioRecordApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.UpdateButton.clicked.connect(update_frame)
-        self.DeleteButton.clicked.connect(print_frame_array)
+        self.directoryButton.clicked.connect(self.choose_directory)
+        self.deleteButton.clicked.connect(delete_checked)
+
+    def choose_directory(self):
+        obj = Dialog()
+        obj.exec()
+        print(obj.get_directory())
 
 class FileFrame(QtWidgets.QFrame, frame_design.Ui_Frame):
     def __init__(self):
@@ -42,13 +66,12 @@ class FileFrame(QtWidgets.QFrame, frame_design.Ui_Frame):
         self.lineEdit.setText(info)
         # print(self.lineEdit.text())
 
-    def try_to_delete(self):
+    def try_to_delete_frames(self):
         if self.radioButton.isChecked():
             self.deleteLater()
             os.remove('%s\\%s' % (directory, self.lineEdit.text()))
             frame_array.remove(self)
         pass
-
 
 def update_frame():
     global window
