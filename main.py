f6647a928b3cede97dc8a6cdc9121d4ec74bdd68
Kassir322 <Kassir3222@gmail.com>
2021-02-19 15:52:42 +0300

    Организовал функции. Теперь каждый класс обладает надлежащими функциями

--- main.py
+++ main.py
@@ -7,96 +7,63 @@ from PyQt5 import QtWidgets
 import main_window_design as design # Это наш конвертированный файл дизайна
 import file_frame as frame_design
 
-# GLOBAL variables
-frame_count = 0 # счетчик добавленных файлов
-frame_array = [] # массив, содержащий в себе экземпляры класса FileFrame
-directory = False
-
-def delete_checked():
-    global frame_array
-    global directory
-
-    for j in range(2): # без двойного прохода почему-то не удаляются сразу 
-        for elem in frame_array: # все элементы
-            elem.try_to_delete_frames()
-
-class Dialog(QtWidgets.QFileDialog):
-    global window
-    
-    def __init__(self):
-        super().__init__()
-        self.__filters = ["All fileeees (*.*)"]
-        self.__default_filter_index = 0
-        self.__directory = ''
-
-    
-    def exec(self):
-        _filter = self.__filters[self.__default_filter_index]
-        self.setNameFilters(self.__filters)
-        self.selectNameFilter(_filter)
-        self.__directory = self.getExistingDirectory(self, 'Выберите папку', options=self.DontUseNativeDialog)
-
-
-    def get_directory(self):
-        return self.__directory
-
 class AudioRecordApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
         self.directoryButton.clicked.connect(self.choose_directory)
-        self.deleteButton.clicked.connect(delete_checked)
+        self.deleteButton.clicked.connect(self.delete_checked)
+        self.directory = 'D:/python/GUI_Application/audio'
+        self.update_frame()
 
     def choose_directory(self):
-        obj = Dialog()
-        obj.exec()
-        print(obj.get_directory())
+        dialog = QtWidgets.QFileDialog(self)
+        self.directory = dialog.getExistingDirectory(self, 'Выберите папку', options=dialog.DontUseNativeDialog)
+        self.update_frame()
+
+    def print_dir(self):
+        print(self.directory)
+
+    def clear_scroll_area(self):
+        while self.verticalLayout_5.count() > 0:
+                item = self.verticalLayout_5.takeAt(0)
+                check = item.widget()
+                if check:
+                    check.deleteLater()
+    
+    def update_frame(self):
+        self.clear_scroll_area()
+
+        if self.directory:
+            for file_name in os.listdir(self.directory):
+                if file_name[-3:] == 'm4a':
+                    self.frame = FileFrame()
+                    self.frame.set_caption(file_name)
+                    self.verticalLayout_5.addWidget(self.frame)
+
+    def delete_checked(self):
+        # print(self.scrollAreaWidgetContents_2.findChildren(QtWidgets.QFrame))
+        for elem in self.scrollAreaWidgetContents_2.findChildren(QtWidgets.QFrame):
+            if elem.objectName() == 'frame':
+                elem.try_to_delete_frames(self.directory)
 
 class FileFrame(QtWidgets.QFrame, frame_design.Ui_Frame):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
 
-        global frame_count
-        frame_count += 1
-        self.setObjectName('frame%d' % frame_count)
+        self.setObjectName('frame')
         # print(self.objectName()) 
 
     def set_caption(self, info: str):
         self.lineEdit.setText(info)
         # print(self.lineEdit.text())
 
-    def try_to_delete_frames(self):
+    def try_to_delete_frames(self, directory):
         if self.radioButton.isChecked():
             self.deleteLater()
-            os.remove('%s\\%s' % (directory, self.lineEdit.text()))
-            frame_array.remove(self)
+            # os.remove('%s\\%s' % (directory, self.lineEdit.text()))
         pass
-
-def update_frame():
-    global window
-    global frame_count
-    global frame_array
-    frame_count = 0
-
-    while window.verticalLayout_5.count() > 0:
-        item = window.verticalLayout_5.takeAt(0)
-        check = item.widget()
-        if check:
-            check.deleteLater()
-
-    global directory
-    directory = False
-    directory = QtWidgets.QFileDialog.getExistingDirectory(window, 'choose folder')
-
-    if directory:
-        for file_name in os.listdir(directory):
-            if file_name[-3:] == 'm4a':
-                window.frame = FileFrame()
-                window.frame.set_caption(file_name)
-                # ТУТ ДОБАВЛЯТЬ В МАССИВ window.frame
-                frame_array.append(window.frame)
-                window.verticalLayout_5.addWidget(window.frame)
 
 def main():
     global window
